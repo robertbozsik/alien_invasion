c85c17cb92b473c0286f16de998f3db47da15f8b
robertbozsik <robertbozsik@gmail.com>
2021-08-18 16:19:22 +0200

    creating a row of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -90,8 +90,21 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
+        # Create an alien and find the number of aliens in a row
+        # Spacing between each alien is equal to one alien width
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width  # e.g. 50 px
+        # available_space_x = self.screen.get_rect().width - (2 * alien_width)
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Crate the first row of aliens
+        for alien_number in range(number_aliens_x):
+            # Create an alien and place it in the row
+            alien = Alien(self)
+            alien.x = alien_width + ((2 * alien_width) * alien_number)
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     # a single leading underscore indicates a helper method
     def _update_screen(self):
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         "Initialize the game's settings."
         # Screen settings
-        self.screen_width = 600
+        self.screen_width = 1000
         self.screen_height = 400
         self.bg_color = (230, 230, 230)  # light grey
 
